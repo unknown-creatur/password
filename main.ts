d44671c1953998597dadd1af1b16c40cfb9ef57a
421385 <115143096+unknown-creatur@users.noreply.github.com>
2023-03-26 21:32:45 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,31 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
     basic.pause(3000)
+    while (判斷值 == 0) {
+        if (input.buttonIsPressed(Button.A)) {
+            數 += 1
+            basic.pause(100)
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            數 += -1
+            basic.pause(100)
+        }
+        if (input.buttonIsPressed(Button.AB)) {
+            數 += 數 * 2
+            basic.pause(100)
+        }
+        if (input.isGesture(Gesture.LogoDown)) {
+            數 = 0
+            basic.pause(100)
+        }
+        if (input.isGesture(Gesture.Shake)) {
+            數 = randint(-1e+30, 1e+30)
+            basic.pause(100)
+        }
+        if (input.isGesture(Gesture.ScreenDown)) {
+            攔截數()
+        }
+    }
 })
 input.onGesture(Gesture.ScreenDown, function () {
 	
@@ -8,36 +33,22 @@ input.onGesture(Gesture.ScreenDown, function () {
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
     basic.pause(3000)
-    radio.sendString(receivedString)
+    if (判斷值 == 1) {
+        radio.sendString("this message had been intercepted!")
+    } else {
+        radio.sendString(receivedString)
+    }
 })
 function 攔截數 () {
     radio.sendNumber(數)
 }
-function 強力攔截數 () {
-    radio.sendNumber(randint(-1e+30, 1e+30))
-}
+let 判斷值 = 0
 let 數 = 0
 radio.setGroup(1)
+數 = 0
+判斷值 = 0
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        數 += 1
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        數 += -1
-    }
-    if (input.buttonIsPressed(Button.AB)) {
-        數 = 數 * randint(1, 100)
-    }
-    if (input.isGesture(Gesture.LogoDown)) {
-        數 = 0
-    }
-    if (input.isGesture(Gesture.Shake)) {
-        數 = randint(-1e+30, 1e+30)
-    }
-    if (input.isGesture(Gesture.ScreenDown)) {
-        攔截數()
-    }
-    basic.pause(100)
+	
 })
 basic.forever(function () {
     basic.showNumber(數)
